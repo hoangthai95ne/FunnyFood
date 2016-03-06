b1ca6ab1a84043b851d09479ac06f993aedbc59b
IM Hoang Thai <hoangthai95ne@gmail.com>
2016-03-06 14:13:36 +0700

    Initial Commit

---
+++ FunnyFood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunnyFood
+//
+//  Created by Hoàng Thái on 3/6/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
